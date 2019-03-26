345f1e85deb1f5f185ee6070bbbee5141d01b6b5
Farin <toucanmeister@gmail.com>
2019-03-26 10:31:12 +0100

    Start funktioniert schonmal.

--- angular-httpclient/src/app/app.component.ts
+++ angular-httpclient/src/app/app.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {StartService} from './start.service';
+import {OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-httpclient';
+export class AppComponent implements OnInit{
+  size: number;
+  url: string = "http://localhost:8081/minesweeper-service/";
+  gameStarted: boolean = false;
+  cellOpacity: number = 0.3;
+  numberOfRows: number = 8;
+  cellColor = [];
+  neighborMines = [];
+  numOfMines: number = 10;
+  alive: boolean = false;
+  winner: boolean = false;
+
+
+  constructor(public Starter: StartService) {
+  }
+
+  ngOnInit(){
+    this.size = 64;
+    let i: number;
+    for(i = 0; i < this.size; i++){
+      this.cellColor[i] = "lightgrey";
+      this.neighborMines[i] = "";
+    }
+  }
+
+  counter(i: number){
+    let arr = new Array(i);
+    let j: number;
+
+    for(j = 0; j < i; j++){
+      arr[j]=j;
+    }
+    return arr;
+  }
+
+  startGame(rowSize: number, numOfMines: number){
+
+    if(rowSize === undefined) rowSize = 8;
+    if(numOfMines === undefined) numOfMines = 10;
+
+    this.Starter.createBoard(rowSize, numOfMines)
+      .subscribe(data => this.numberOfRows = data.numberOfRows);
+
+    this.size = this.numberOfRows * this.numberOfRows;
+    this.gameStarted = true;
+    this.cellOpacity = 1;
+    this.numOfMines = numOfMines;
+
+    let i: number;
+    for(i = 0; i < this.size; i++){
+      this.cellColor[i] = "lightgrey";
+      this.neighborMines[i] = "";
+    }
+
+    console.log(rowSize, numOfMines, this.numberOfRows, this.size)
+  }
+
+  clickCell(cellNum: number){
+    if(this.gameStarted && this.cellColor[cellNum] !== "tomato"){
+    }
+  }
+
+  flagCell(cellNumber: number){
+
+  }
 }
---
+++ angular-httpclient/src/app/start.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+
+interface Start {
+  size: number;
+  numberOfRows: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StartService {
+  start: Start;
+
+  constructor(private http:HttpClient) { }
+
+  createBoard(rowSize: number, numOfMines: number): Observable<Start>{
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    return this.http
+      .post<Start>("http://localhost:8081/minesweeper-service/start","{\"rowSize\": " + rowSize + ", \"numOfMines\": " + numOfMines + "}", httpOptions)
+      .pipe(map(data => {
+        this.start = data;
+        return this.start;
+        }
+      ))
+  }
+}

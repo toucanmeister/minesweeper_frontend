7d0a70c20f4cf23b0a7f8841279c3154f80a2665
Farin <toucanmeister@gmail.com>
2019-03-26 12:54:27 +0100

    Klicken implementiert

--- angular-httpclient/src/app/app.component.ts
+++ angular-httpclient/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {StartService} from './start.service';
+import {ClickService} from './click.service';
 import {OnInit} from '@angular/core';
 
 
@@ -10,18 +11,20 @@ import {OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit{
   size: number;
-  url: string = "http://localhost:8081/minesweeper-service/";
-  gameStarted: boolean = false;
-  cellOpacity: number = 0.3;
   numberOfRows: number = 8;
-  cellColor = [];
-  neighborMines = [];
   numOfMines: number = 10;
-  alive: boolean = false;
+
+  cellOpacity: number = 0.3;
+  cellColor = [];
+
+  neighborMines = [];
+
+  gameStarted: boolean = false;
+  alive: boolean = true;
   winner: boolean = false;
 
 
-  constructor(public Starter: StartService) {
+  constructor(public Starter: StartService, public Clicker: ClickService) {
   }
 
   ngOnInit(){
@@ -61,16 +64,51 @@ export class AppComponent implements OnInit{
       this.cellColor[i] = "lightgrey";
       this.neighborMines[i] = "";
     }
-
-    console.log(rowSize, numOfMines, this.numberOfRows, this.size)
   }
 
   clickCell(cellNum: number){
+
     if(this.gameStarted && this.cellColor[cellNum] !== "tomato"){
+      this.Clicker.sendClick(cellNum)
+        .subscribe( data => {
+          console.log(data);
+          if(data.okay) {
+            this.alive = data.alive;
+            this.winner = data.winner;
+            this.displayClickAndNeighborMines(cellNum, data.clickedCells, data.numOfNeighborMines);
+          }
+          if(!(this.alive) || this.winner){
+            this.gameStarted = false;
+            this.cellOpacity = 0.3;
+            this.showMines(data.mines);
+          }
+        })
     }
   }
 
   flagCell(cellNumber: number){
 
+  }
+
+  displayClickAndNeighborMines(cellNum: number, clickedCells: number[], numOfNeighborMines: number[]){
+    let i: number;
+    for(i = 0; i < clickedCells.length; i++){
+      let currentClickedCell = clickedCells[i];
+      this.cellColor[currentClickedCell] = "darkgrey";
+      if(numOfNeighborMines[i] === 0) {
+        this.neighborMines[currentClickedCell] = "";
+      } else {
+        this.neighborMines[currentClickedCell] = numOfNeighborMines[i].toString();
+      }
+    }
+  }
+
+  showMines(mines: number[]){
+    let i: number;
+    for(i = 0; i < this.size; i++){
+      if(mines.includes(i)){
+        this.cellColor[i] = "firebrick";
+      }
+    }
   }
 }
--- angular-httpclient/src/app/click.service.ts
+++ angular-httpclient/src/app/click.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import {HttpHeaders, HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+
+interface Click {
+  alive: boolean;
+  winner: boolean;
+  numOfNeighborMines: [];
+  clickedCells: [];
+  mines: [];
+  okay: boolean;
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClickService {
+  click: Click;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  sendClick(cellNum: number){
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    return this.http
+      .post<Click>("http://localhost:8081/minesweeper-service/click","{\"cellNum\": " + cellNum + "}", httpOptions)
+      .pipe(map(data => {
+          this.click = data;
+          return this.click;
+        }
+      ))
+  }
 }
